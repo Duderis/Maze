2d93ea58bba35039a576a15fc969d9b44b2200d6
Marius Ambrazevicius <Duderis@bitbucket.org>
2016-05-11 22:07:48 +0300

    fixed mp camera

---
+++ Assets/Script/PlayerInitialize.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class PlayerInitialize : NetworkBehaviour {
+	public GameObject cam;
+	// Use this for initialization
+	void Start () {
+		if (isLocalPlayer){
+			GameObject maincamera = Instantiate (cam);
+			maincamera.transform.parent = this.gameObject.transform;
+		}
+	}
+}
