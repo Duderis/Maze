44b5975bd3ec2d2fd7fc965124065357652918ad
Marius Ambrazevicius <Duderis@bitbucket.org>
2016-05-25 18:19:18 +0300

    added IP adress validation, and name saving into player prefs

--- Assets/Script/stuff.cs
+++ Assets/Script/stuff.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Net;
 using UnityEngine.Networking;
 using UnityEngine.UI;
 
@@ -10,12 +11,23 @@ public class stuff : NetworkBehaviour {
 		nm.StartServer ();
 	}
 	public void networkJoin () {
+		getName ();
 		InputField input = GameObject.Find ("InputField").GetComponent<InputField> ();
-		nm.networkAddress = "88.222.117.174";//input.text;
+		IPAddress adress;
+		nm.networkAddress = input.text;
+		if (!(IPAddress.TryParse (nm.networkAddress, out adress))) {
+			nm.networkAddress = "88.222.117.174";
+		}
 		nm.networkPort = 7777;
 		nm.StartClient ();
 	}
 	public void networkHost () {
 		nm.StartHost ();
+		getName ();
+	}
+	public void getName(){
+		InputField name = GameObject.Find ("UserInput").GetComponent<InputField> ();
+		PlayerPrefs.SetString ("Player Name", name.text);
+		Debug.Log (name.text);
 	}
 }
